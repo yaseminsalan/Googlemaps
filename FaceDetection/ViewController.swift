b33e5a1d10f3991dc25af7a44079931191629505
yaseminsalan <yaseminsalann@gmail.com>
2020-03-30 18:48:07 +0300

    Initial Commit

---
+++ FaceDetection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceDetection
+//
+//  Created by Yasemin salan on 30.03.2020.
+//  Copyright © 2020 Yasemin salan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FaceDetectionTests/FaceDetectionTests.swift
@@ -0,0 +1,34 @@
+//
+//  FaceDetectionTests.swift
+//  FaceDetectionTests
+//
+//  Created by Yasemin salan on 30.03.2020.
+//  Copyright © 2020 Yasemin salan. All rights reserved.
+//
+
+import XCTest
+@testable import FaceDetection
+
+class FaceDetectionTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
